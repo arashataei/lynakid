fc63b1dd26bc518f1f13d4d75fbfec9d52858c35
cthunter <idev.softwaregroup@gmail.com>
2025-01-24 04:51:54 +0330

    initial commit - #2 - removed footer folder and file plsu removed the footer content

--- src/components/HomeSection/HomeSection.jsx
+++ src/components/HomeSection/HomeSection.jsx
@@ -159,15 +159,6 @@ const HomeSection = () => {
               </div>
             </div>
           </div>
-          <div className="ms-copyrights">
-            <div className="ms-copy">
-              حق نشر © <a className="site-name" href="index.html">Masterkart</a> همه
-              حقوق محفوظ است پشتیبانی شده توسط Masterkart.
-            </div>
-            <div className="ms-payment">
-              <img src="assets/img/hero-bg/payment.png" alt="payment" />
-            </div>
-          </div>
         </div>
       </div>
     </section>
